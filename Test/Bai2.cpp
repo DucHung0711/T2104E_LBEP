a5cb0858510ff551dacd081e8c0a621b2ea3dd82
DucHung0711 <hnguyenduchung014@gmail.com>
2021-06-05 15:40:01 +0700

    Baithi

--- Test/Bai2.cpp
+++ Test/Bai2.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-	char s[50];
-	printf("Nhap vao chuoi: ");
-	scanf("%s",s);	
+void upperCase (char s[]){	
 	for(int i=0;i<=strlen(s);i++){
       if(s[i]>=97&&s[i]<=122){
       	 s[i]=s[i]-32;
 		}   
 	}
-	printf("Mang sau duoc in thanh in hoa la %s ",s);  
+	printf("Mang sau duoc in thanh in hoa la %s ",s); 
+}
+int main(){
+	char s[50];
+	printf("Nhap vao chuoi: ");
+	scanf("%s",s);
+	upperCase(s);
 }
